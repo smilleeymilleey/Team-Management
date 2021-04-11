80eb92d5325c4730f349e866c8478541334a092f
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-11 13:43:13 -0500

    added a git ignore

---
+++ index.js
@@ -0,0 +1,35 @@
+// download npm and inquirer
+//use inquirer to ask what they want to do
+    //prompt to enter team managers name, id, email and officenumber
+
+        //prompt again with options: 
+            //add engineer
+                //prompted to enter:
+                    //name
+                    //id
+                    //email
+                    //github
+                // then go back to main menu main menu
+
+            //add intern
+                //name, id, email, school
+                //then main menu
+
+            //Finished building team
+                //generate HTML
+                    //generate HTML that displays a team roster
+                            //click on email address => pops open email with to: filled out address
+                            //click on github username => profile opens in new tab
+                //exit application
+
+     
+
+
+
+
+
+
+
+
+
+
